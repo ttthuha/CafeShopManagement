e9b4bc159753552f960fde91851b3ac6bf7820d2
DiepNV4 <diepnv4@fsoft.com.vn>
2018-10-10 23:29:35 +0700

    Fix DbConnection

--- CafeShop/Data/FoodRepository.cs
+++ CafeShop/Data/FoodRepository.cs
@@ -6,13 +6,20 @@ using System.Linq;
 using System.Threading.Tasks;
 using Dapper;
 
+using Microsoft.Extensions.Configuration;
+
 namespace CafeShop.Data
 {
     public class FoodRepository
     {
+        private readonly string connectionString = null;
+        public FoodRepository(IConfiguration configuration)
+        {
+            connectionString = configuration["DbConnection"];
+        }
         public IEnumerable<FoodViewModel> Get()
         {
-            using (SqlConnection connection = new SqlConnection("Server=.\\sqlexpress;Database=CafeShopManagement;Trusted_Connection=True;"))
+            using (SqlConnection connection = new SqlConnection(connectionString))
             {
                 return connection.Query<FoodViewModel>("select Id, Name, Price, Type from Food order by Name");
             }
